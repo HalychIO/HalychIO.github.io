4530ce532376161c34c527c2a324f071b66fd992
Halych <HalychIO3@gmail.com>
2024-05-03 10:32:27 +0300

    add render NavButtons and Pages

---
+++ script/data.js
@@ -0,0 +1,87 @@
+export let dataContent = {
+    UA: {
+        "home": {
+            'h1': "ГОЛОВНА",
+            "content": [
+                {
+                    "h2": "h2 №1",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!",
+                    "img": "../img/bodyBackround.jpg"
+
+                },
+                {
+                    "h2": "h2 №1",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                }
+            ]
+        },
+        "aboutUs": {
+            'h1': "ПРО НАС",
+            "content": [
+                {
+                    "h2": "h2 №2",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                },
+                {
+                    "h2": "h2 №2",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                }
+            ]
+        },
+        "contact": {
+            'h1': "КОНТАКТИ",
+            "content": [
+                {
+                    "h2": "h2 №3",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                },
+                {
+                    "h2": "h2 №3",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                }
+            ]
+        }
+    },
+    EN: {
+        "home": {
+            'h1': "ГОЛОВНА",
+            "content": [
+                {
+                    "h2": "h2 №1",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                },
+                {
+                    "h2": "h2 №1",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                }
+            ]
+        },
+        "aboutUs": {
+            'h1': "ПРО НАС",
+            "content": [
+                {
+                    "h2": "h2 №1",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                },
+                {
+                    "h2": "h2 №1",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                }
+            ]
+        },
+        "contact": {
+            'h1': "КОНТАКТИ",
+            "content": [
+                {
+                    "h2": "h2 №1",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                },
+                {
+                    "h2": "h2 №1",
+                    "p": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore possimus hic, adipisci neque quae ipsa veniam sint. Dignissimos eaque repudiandae perspiciatis fugiat voluptatem, repellat qui adipisci reiciendis fuga ullam quaerat!"
+                }
+            ]
+        }
+    }
+}
+
--- script/index.js
+++ script/index.js
@@ -1,18 +1,63 @@
-let stepX = 27.0;
-let stepY = 23.5;
+import { dataContent } from "./data.js";
 
-for (s = -10; s < 10; s++) {
-    for (i = -1; i < 10; i++) {
-        let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
-        path.setAttribute(
-            "d",
-            `M ${Boolean(s % 2) ? i * stepX + 13.5 : i * stepX
-            } ${s * stepY} l 13 7.5 l 0 15 l -13.0 7.5 l -13 -7.5 l 0 -15 Z`
-        );
-        // path.setAttribute(
-        //     "fill",
-        //     `url(img/bodyBackround.jpg)`
-        // );
-        document.getElementsByTagNameNS("http://www.w3.org/2000/svg", "svg")[0].appendChild(path);
+let interfaceLanguage = "UA";
+
+(() => {  // рендер фону
+    let stepX = 27.0;
+    let stepY = 23.5;
+
+    for (let s = -10; s < 10; s++) {
+        for (let i = -1; i < 10; i++) {
+            let path = document.createElementNS("http://www.w3.org/2000/svg", "path");
+            path.setAttribute(
+                "d",
+                `M ${Boolean(s % 2) ? i * stepX + 13.5 : i * stepX
+                } ${s * stepY} l 13 7.5 l 0 15 l -13.0 7.5 l -13 -7.5 l 0 -15 Z`
+            );
+            document.getElementsByTagNameNS("http://www.w3.org/2000/svg", "svg")[0].appendChild(path);
+        }
     }
+})();
+
+
+
+function buttonNavRender() {
+    let navBar = document.getElementById('navBar');
+
+    Object.keys(dataContent[interfaceLanguage]).forEach((element) => {
+        let createTeg = document.createElement("button");
+        createTeg.className = "navPoint";
+        createTeg.textContent = dataContent[interfaceLanguage][element].h1;
+        createTeg.onclick = () => openPage(element);
+
+        navBar.appendChild(createTeg);
+    })
 }
+buttonNavRender()
+
+const openPage = (OpenID) => {
+    let divContent = document.getElementById("content");
+    divContent.innerHTML = "";
+
+    dataContent[interfaceLanguage][OpenID].content.forEach(element => {
+        let addElem = Object.keys(element);
+        let div = document.createElement("div");
+        div.className = "contentItem";
+
+        addElem.forEach(elem => {
+            let createTeg = document.createElement(elem);
+
+            elem !== "img" ?
+                createTeg.textContent = element[elem] :
+                createTeg.src = element[elem];
+
+            div.appendChild(createTeg);
+        });
+
+        divContent.appendChild(div);
+    });
+
+
+}
+openPage("home");
+
